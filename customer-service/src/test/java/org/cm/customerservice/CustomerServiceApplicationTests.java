0056b0b8f10638300e76250a46f1bdcc7a7df3dc
Ahmed <ahmedakestaf@gmail.com>
2025-11-09 04:46:45 +0000

    Initial commit: Fisr microservice  project setup

---
+++ customer-service/src/test/java/org/cm/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cm.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
