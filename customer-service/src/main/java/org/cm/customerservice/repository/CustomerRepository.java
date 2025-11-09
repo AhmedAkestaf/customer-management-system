c4b31c13e1d48c7c46f04ab80502b1103028004d
Ahmed <ahmedakestaf@gmail.com>
2025-11-09 09:13:50 +0000

    Creating the  Customer Repository

---
+++ customer-service/src/main/java/org/cm/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package org.cm.customerservice.repository;
+
+import org.cm.customerservice.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+
+}
+
