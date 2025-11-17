6ccccf8e37c4a0103c2cf82a820cb2e805ae1ad9
Ahmed <ahmedakestaf@gmail.com>
2025-11-17 17:40:48 +0000

    a few modifications

---
+++ auth-service/src/main/java/org/cm/authservice/service/UserService.java
@@ -0,0 +1,19 @@
+package org.cm.authservice.service;
+
+import org.cm.authservice.model.User;
+import org.cm.authservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
--- customer-management-ui/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { CustomerList } from './features/customers/customer-list/customer-list';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/customers', pathMatch: 'full' },
-  { path: 'customers', component: CustomerList },
-  { path: '**', redirectTo: '/customers' }
-];
